c01d3260eb509efcec95eef3f7dda4f541247e26
(no author) <(no author)@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2009-04-17 13:32:54 +0000

    adding test file
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@6 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

---
+++ sources/fools/test.c
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include <model.h>
+#include <bootstrap.h>
+#include <stdio.h>
+
+#define SETUP\
+    fools_object system = bootstrap();
+
+void test_array() {
+    SETUP;
+
+    array_object a = make_array(system, 100);
+    assert(a->size->value == 100);
+    int i;
+    for(i = 0; i < 100; i++) {
+        a->values[i] = (object)make_number(system, i);
+    }
+    
+    for(i = 0; i < 100; i++) {
+        assert(a->values[i].number->value == i);
+    }
+}
+
+void native_test_single_arg_5(context_object c) {
+    assert(c->arguments->size->value == 1);
+    assert(c->arguments->values[0].number->value == 5);
+}
+
+void test_native() {
+    SETUP;
+
+    native_object n = make_native(&native_test_single_arg_5);
+    header(n) = (object)system->native;
+
+    context_object c = make_context(system, (object)n, 1);
+    c->arguments->values[0] = (object)make_number(system, 5);
+
+    native(c);
+}
+
+int main() {
+
+    test_array();
+    //test_native();   
+
+    return 0;
+    
+}
