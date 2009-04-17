1bf612534013bb680ff60b9969a6fd6ed404ca15
(no author) <(no author)@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2009-04-17 16:53:03 +0000

    cleanup
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@17 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- sources/fools/bootstrap.c
+++ sources/fools/bootstrap.c
@@ -13,28 +13,9 @@ fools_object bootstrap() {
     fools_system->nil               = nil;
     //system->behaviour_class       = make_class(5, (object)nil, "Behaviour");
     //system->class_class           = make_class(5, (object)nil, "Class");
-    native_object native_f          = make_native(&native);
-    fools_system->native            = native_f;
+    fools_system->native            = make_native(&native);
  
-    printf("add: %x\n", &native);
-    printf("header: %x\n", (object)fools_system->native);
-    printf("header: %x\n", (object)fools_system->native);
-
-    int i;
-    for (i = 0; i < 10; i++) {
-      printf("v: %x\n", *((int*)(fools_system->native)+i-3));
-    }
-
-    assert(fools_system->native->function == &native);
-
     header(fools_system->native)    = (object)fools_system->native;
-
-    printf("new: \n\n");
-
-    for (i = 0; i < 10; i++) {
-      printf("v: %x\n", *((int*)(fools_system->native)+i-3));
-    }
-
 
     assert(fools_system->native->function == &native);
 
