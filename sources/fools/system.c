9f7c633252ee74aa8dcaf47c8272521118ce8242
tverwaes <tverwaes@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2009-04-25 20:57:34 +0000

    adding abstractions for arguments from a context
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@85 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- sources/fools/system.c
+++ sources/fools/system.c
@@ -6,6 +6,19 @@
 
 #include <stdio.h>
 
+// Helper functions
+
+void inline set_message(context_object context, const char* value) {
+    array_at_put(context->arguments, 0, symbol_known_to_the_vm(value));
+}
+
+void inline set_argument(context_object context, int index, object value) {
+    array_at_put(context->arguments, index, value);
+}
+
+object inline argument_at(context_object context, int index) {
+    return array_at(context->arguments, index);
+}
 
 // Context handling
 
@@ -17,7 +30,7 @@ void inline return_from_context(context_object context) {
 }
 
 context_object inline target_context(context_object interpreter_context) {
-    return array_at(interpreter_context->arguments, 1).context;
+    return argument_at(interpreter_context, 1).context;
 }
 
 
@@ -49,12 +62,12 @@ void ilist_eval(context_object context) {
         return return_from_context(ilist_context);
     }
 
-    object environment = array_at(ilist_context->arguments, 1);
+    object environment = argument_at(ilist_context, 1);
 
     ilist_context->arguments = make_array(4);
-    array_at_put(ilist_context->arguments, 0, symbol_known_to_the_vm("return:env:continue:"));
-    array_at_put(ilist_context->arguments, 2, environment);
-    array_at_put(ilist_context->arguments, 3, (object)make_number(0));
+    set_message(ilist_context, "return:env:continue:");
+    set_argument(ilist_context, 2, environment);
+    set_argument(ilist_context, 3, (object)make_number(0));
 
     transfer(ilist_context);
 }
@@ -62,7 +75,7 @@ void ilist_eval(context_object context) {
 // ilist>>return:env:continue:
 void ilist_continue_eval(context_object context) {
     context_object ilist_context = target_context(context);
-    int index = number_value(array_at(ilist_context->arguments, 3).number);
+    int index = number_value(argument_at(ilist_context, 3).number);
     ilist_object ilist = header(ilist_context).instruction.ilist;
     int size = number_value(ilist->size) - 1;
 
@@ -70,14 +83,14 @@ void ilist_continue_eval(context_object context) {
     header(context) = instruction;
 
     if (index != size) {
-        array_at_put(ilist_context->arguments, 3, (object)make_number(index + 1));
+        set_argument(ilist_context, 3, (object)make_number(index + 1));
         context->return_context = (object)ilist_context;
     } else { // tailcall.
         context->arguments = make_array(2);
         context->return_context = (object)ilist_context->return_context;
     }
     
-    array_at_put(context->arguments, 0, symbol_known_to_the_vm("eval:"));
+    set_message(context, "eval:");
 
     transfer(context);
 }
@@ -87,8 +100,8 @@ void iconst_eval(context_object context) {
     context_object iconst_context = target_context(context);
     iconst_object iconst = header(iconst_context).instruction.iconst;
 
-    array_at_put(iconst_context->return_context.context->arguments, 1,
-                 iconst->constant);
+    set_argument(iconst_context->return_context.context,
+                 1, iconst->constant);
 
     return_from_context(iconst_context);
 }
@@ -110,16 +123,15 @@ void iassign_eval(context_object context) {
     iassign_object iassign = header(iassign_context).instruction.iassign;
     
 
-    env_object env = array_at(iassign_context->arguments, 1).env;
+    env_object env = argument_at(iassign_context, 1).env;
 
     header(context)     = iassign->expression;
     context->arguments  = iassign_context->arguments;
 
     header(iassign_context)    = (object)(instruction)iassign->variable;
     iassign_context->arguments = make_array(3);
-    array_at_put(iassign_context->arguments, 0,
-                 symbol_known_to_the_vm("assign:in:"));
-    array_at_put(iassign_context->arguments, 2, (object)env);
+    set_message(iassign_context, "assign:in:");
+    set_argument(iassign_context, 2, (object)env);
 
     context->return_context = (object)iassign_context;
 
@@ -131,17 +143,16 @@ void ivar_assign(context_object context) {
     context_object ivar_context = target_context(context);
     ivar_object ivar = header(ivar_context).instruction.ivar;
 
-    object env = array_at(ivar_context->arguments, 2);
+    object env = argument_at(ivar_context, 2);
 
-    object value = array_at(ivar_context->arguments, 1);
+    object value = argument_at(ivar_context, 1);
     
     header(ivar_context) = env;
     ivar_context->arguments = make_array(4);
-    array_at_put(ivar_context->arguments, 0,
-                 symbol_known_to_the_vm("store:at:in:"));
-    array_at_put(ivar_context->arguments, 1, value);
-    array_at_put(ivar_context->arguments, 2, (object)ivar->index);
-    array_at_put(ivar_context->arguments, 3, ivar->scope);
+    set_message(ivar_context, "store:at:in:");
+    set_argument(ivar_context, 1, value);
+    set_argument(ivar_context, 2, (object)ivar->index);
+    set_argument(ivar_context, 3, ivar->scope);
 
     transfer(ivar_context);
 }
@@ -151,14 +162,13 @@ void ivar_eval(context_object context) {
     context_object ivar_context = target_context(context);
     ivar_object ivar = header(ivar_context).instruction.ivar;
 
-    object env = array_at(ivar_context->arguments, 1);
+    object env = argument_at(ivar_context, 1);
 
     header(ivar_context) = env;
     ivar_context->arguments = make_array(3);
-    array_at_put(ivar_context->arguments, 0,
-                 symbol_known_to_the_vm("fetch:from:"));
-    array_at_put(ivar_context->arguments, 1, (object)ivar->index);
-    array_at_put(ivar_context->arguments, 2, ivar->scope);
+    set_message(ivar_context, "fetch:from:");
+    set_argument(ivar_context, 1, (object)ivar->index);
+    set_argument(ivar_context, 2, ivar->scope);
 
     transfer(ivar_context);
 }
@@ -168,14 +178,14 @@ void iscoped_eval(context_object context) {
     context_object iscoped_context = target_context(context);
     iscoped_object iscoped = header(iscoped_context).instruction.iscoped;
 
-    object dynamic_env = array_at(iscoped_context->arguments, 1);
+    object dynamic_env = argument_at(iscoped_context, 1);
     object static_env  = iscoped->scope;
 
     context_object return_context = iscoped_context->return_context.context;
 
     header(iscoped_context) = iscoped->expression;
-    array_at_put(iscoped_context->arguments, 1, static_env);
-    array_at_put(return_context->arguments, 2, dynamic_env); 
+    set_argument(iscoped_context, 1, static_env);
+    set_argument(return_context, 2, dynamic_env); 
 
     transfer(iscoped_context);
 }
@@ -184,10 +194,9 @@ void iscoped_eval(context_object context) {
 void icapture_eval(context_object context) {
     context_object icapture_context = target_context(context);
 
-    object dynamic_env = array_at(icapture_context->arguments, 1);
+    object dynamic_env = argument_at(icapture_context, 1);
 
-    array_at_put(icapture_context->return_context.context->arguments, 1,
-                 dynamic_env);
+    set_argument(icapture_context->return_context.context, 1, dynamic_env);
 
     return_from_context(icapture_context);
 }
@@ -199,8 +208,7 @@ void with_native_class_lookup(context_object context) {
     context_object receiver_context = target_context(class_context);
     native_class_object class       = header(class_context).native_class;
     dict_object natives             = class->natives;
-    array_object arguments          = receiver_context->arguments;
-    object selector                 = array_at(arguments, 0);
+    object selector                 = argument_at(receiver_context, 0);
     object native                   = dict_at(natives, selector);
 
     if (native.nil == fools_system->nil) {
