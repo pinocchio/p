bc142ea4244ed1663772edc5046d94e6993f6695
tverwaes <tverwaes@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2009-04-29 11:23:48 +0000

    removing old C function headers
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@112 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- sources/fools/primitives.h
+++ sources/fools/primitives.h
@@ -9,13 +9,4 @@ extern void prim_dict_at(context_object context);
 extern void prim_dict_at_put(context_object context);
 */
 
-extern void prim_env_fetch_from(context_object context);
-extern void prim_env_store_at_in(context_object context);
-extern void prim_env_subscope(context_object context);
-extern void prim_env_parent(context_object context);
-extern void prim_env_set_parent(context_object context);
-
-extern void prim_iscope_new(context_object context);
-extern void prim_iscoped_scope(context_object context);
-
 #endif // PRIMITIVES_H
