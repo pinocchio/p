e31296861e6f25651bc2e082a61f73e7a5791447
tverwaes <tverwaes@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2009-05-02 15:16:35 +0000

    I can now return a constant 42 :)
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@138 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- sources/fools/postfix.c
+++ sources/fools/postfix.c
@@ -1,17 +1,28 @@
 
 /* end of generated code */
 
-    object themain = (object)make_iconst(lambda_1_x);
-    appcall_object maincall = make_appcall(themain, 0);
-    
-    context_object make_eval_context(ci, maincall, env); 
+    context_object make_eval_context(ci, lambda_1_x, env);
     build_return(ci, rc);
+    transfer(ci);
+
+    object scoped_main = return_value(rc);
+
+    assert(header(scoped_main.pointer).native_class ==
+           fools_system->iscope_class);
+
+    object const_scoped_main = (object)make_iconst(scoped_main);
+
+    appcall_object maincall = make_appcall(const_scoped_main, 0);
     
+    make_eval_context(ci, maincall, env); 
+    ci->return_context = (object)rc;
+    
+    printf("------- Starting eval ------\n");
     transfer(ci);
 
     object result = return_value(rc);
 
-    printf("result: %i", result.number->value);
+    printf("result: %i\n", result.number->value);
 
     return 0;
 }
