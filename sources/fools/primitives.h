0588d05fe5905485dfa9cfb596025b5176f37d96
tverwaes <tverwaes@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2009-05-11 14:20:12 +0000

    removing unused crap.
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@200 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- sources/fools/primitives.h
+++ sources/fools/primitives.h
@@ -1,13 +1,4 @@
 #ifndef PRIMITIVES_H
 #define PRIMITIVES_H
 
-/*
-void number_env_plus(context_object context);
-void number_env_minus(context_object context);
-void number_env_times(context_object context);
-void number_env_divide(context_object context);
-
-void true_env_if(context_object context);
-void false_env_if(context_object context);
-*/
 #endif // PRIMITIVES_H
