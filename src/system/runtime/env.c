1cfcf0eb5b6e9c8973def4b92a401e0537cfb471
tverwaes <tverwaes@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2009-07-15 15:14:57 +0000

    adding cast-tests to all functions in env
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@449 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- src/system/runtime/env.c
+++ src/system/runtime/env.c
@@ -3,8 +3,6 @@
 
 // env>>fetch:from:
 static void inline env_fetch_from() {
-    // XXX Breaking encapsulation without testing.
-    // Test arguments!
     context_object receiver = get_context();
     assert_argsize(receiver, 3);
     // arguments at: 0 -> selector
@@ -12,9 +10,9 @@ static void inline env_fetch_from() {
     debug("env>>fetch:from: (%i) %p\n", env->values->size, env);
 
     if (env->scope.pointer == argument_at(receiver, 2).pointer) {
-        int index = argument_at(receiver, 1).number->value;
-        debug("index: %i\n", index);
-        object value = env_at(env, index);
+        cast(index, argument_at(receiver, 1), number);
+        debug("index: %i\n", index->value);
+        object value = env_at(env, index->value);
         debug("ret>>env>>fetch:from: (return) %p\n", value.pointer);
         return return_from_context(receiver, value);
     }
@@ -26,18 +24,15 @@ static void inline env_fetch_from() {
 
 // env>>store:at:in:
 static void inline env_store_at_in() {
-    // XXX Breaking encapsulation without testing.
-    // Test arguments!
     debug("env>>store:at:in:\n");
     context_object receiver = get_context();
     assert_argsize(receiver, 4);
-    // arguments at: 0 -> selector
     runtime_env_object env = receiver->self.env;
     if (env->scope.pointer == argument_at(receiver, 3).pointer ) {
-        int index = argument_at(receiver, 2).number->value;
+        cast(index, argument_at(receiver, 2), number)
         object value = argument_at(receiver, 1);
-        env_at_put(env, index, value);
-        debug("ret>>env>>store:at:in: %i, %p\n", index, value.pointer);
+        env_at_put(env, index->value, value);
+        debug("ret>>env>>store:at:in: %i, %p\n", index->value, value.pointer);
         return pop_context();
     }
     error_guard(env->parent.object != woodstock->nil, "Variable not found.");
@@ -47,20 +42,17 @@ static void inline env_store_at_in() {
 
 // env>>subScope:key:
 static void inline env_subscope() {
-    // XXX Breaking encapsulation without testing.
-    // Test arguments!
     debug("env>>subScope:key:\n");
     context_object receiver = get_context();
     assert_argsize(receiver, 3);
     object env  = receiver->self;
-    // arguments at: 0 -> selector
-    int size    = argument_at(receiver, 1).number->value;
+    cast(size, argument_at(receiver, 1), number);
     object key  = argument_at(receiver, 2);
 
-    runtime_env_object new_env = make_env(key, env, size);
+    runtime_env_object new_env = make_env(key, env, size->value);
 
     return_from_context(receiver, (object)new_env);
-    debug("ret>>env>>subScope:key: %i, %p\n", size, new_env);
+    debug("ret>>env>>subScope:key: %i, %p\n", size->value, new_env);
 }
 
 // env>>parent:
@@ -77,10 +69,10 @@ with_pre_eval1(env_set_parent, context, new_env,
 // env>>parent
 accessor_for(env, parent)
 
-with_pre_eval2(env_class_scope_key, context, key, w_size,
-    // XXX breaks encapsulation
-    int size    = w_size.number->value;
-	runtime_env_object new_env = make_env(key, (object)woodstock->nil, size);
+with_pre_eval2(env_class_scope_key, context, w_size, key,
+    cast(size, w_size, number);
+	runtime_env_object new_env =
+        make_env(key, (object)woodstock->nil, size->value);
     return_from_context(context, (object)new_env);
 )
 
