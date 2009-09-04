715120a6f8d7d703ceac3e7a417c1a629b0c465b
cami <cami@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2009-09-04 14:23:25 +0000

    added some missing test files
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@700 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

---
+++ p2/pinocchioTest.c
@@ -0,0 +1,48 @@
+
+#include <system/ast/AssignTest.c>
+#include <system/ast/CallecTest.c>
+#include <system/ast/ConstantTest.c>
+#include <system/ast/ContinueTest.c>
+#include <system/ast/MethodTest.c>
+#include <system/ast/NativeMethodTest.c>
+#include <system/ast/SelfTest.c>
+#include <system/ast/SendTest.c>
+#include <system/ast/SuperTest.c>
+#include <system/ast/VariableTest.c>
+#include <system/io/FileTest.c>
+#include <system/runtime/EnvTest.c>
+#include <system/runtime/ThreadTest.c>
+#include <system/type/ArrayTest.c>
+#include <system/type/BooleanTest.c>
+#include <system/type/CharacterTest.c>
+#include <system/type/ClassTest.c>
+#include <system/type/DictionaryTest.c>
+#include <system/type/ObjectTest.c>
+#include <system/type/SmallIntTest.c>
+#include <system/type/StringTest.c>
+
+/* ========================================================================== */
+
+void run_tests()
+{
+    test_Array();
+    test_Assign();
+    test_Callec();
+    test_Constant();
+    test_Continue();
+    test_Method();
+    test_NativeMethod();
+    test_Self();
+    test_Super();
+    test_Send();
+    test_Variable();
+    test_File();
+    test_Boolean();
+    test_Character();
+    test_Class();
+    test_Dictionary();
+    test_Object();
+    test_SmallInt();
+    test_String();
+    test_Thread();
+}
---
+++ p2/system/runtime/ThreadTest.c
@@ -0,0 +1,23 @@
+
+void test_thread_stress()
+{
+    Object test     = (Object)new_Constant((Object)new_SmallInt(0));
+    Object add      = (Object)new_Constant((Object)new_SmallInt(1));
+    
+    // AST_Send * send = new_Send(test, Symbol_plus_, new_Raw_Array(0));
+    AST_Send * send = new_Send(test, Symbol_plus_, new_Array_With(1, add));
+    
+    int idx;
+    int count = 10000000;
+    for (idx = 0; idx < count; idx++) {   
+        //    Eval((Object)assign);
+        Eval((Object)send);
+    }
+}
+
+/* ======================================================================== */
+
+void test_Thread()
+{
+    test_thread_stress();
+}
