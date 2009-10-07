4143cdc9f744192f02c3f7171a7d099e8e86d9ce
cami <cami@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2009-10-07 08:32:32 +0000

    added empty Stream Class
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@817 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

---
+++ p2/system/io/Stream.h
@@ -0,0 +1,19 @@
+#ifndef IO_STREAM_H
+#define IO_STREAM_H
+
+#include <pinocchio.h>
+
+/* ======================================================================== */
+
+struct IO_Stream_t {
+    #error "TODO fill out struct"
+};
+
+CREATE_INITIALIZERS(IO_Stream)
+extern IO_Stream new_IO_Stream();
+
+/* ======================================================================== */
+
+/* ======================================================================== */
+
+#endif // IO_STREAM_H
