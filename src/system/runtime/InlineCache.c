59845d8a30aef1f31c3c008040bdf4d0e5fd22d8
cami <cami@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2010-03-30 15:30:45 +0000

    adding natives for inlinechach
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@1361 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- src/system/runtime/InlineCache.c
+++ src/system/runtime/InlineCache.c
@@ -29,8 +29,20 @@ Runtime_InlineCache new_Runtime_InlineCache()
 
 /* ========================================================================= */
 
-NATIVE1(Runtime_InlineCache_checkCached_) 
-    RETURN_FROM_NATIVE(get_bool(((Runtime_InlineCache)self)->class == NATIVE_ARG(0)));
+NATIVE1(Runtime_InlineCache_checkCached_)
+    if (((Runtime_InlineCache)self)->class == NATIVE_ARG(0)) {
+        RETURN_FROM_NATIVE(((Runtime_InlineCache)self)->method);
+    } else {
+        RETURN_FROM_NATIVE(Nil);
+    }
+}
+
+
+NATIVE2(Runtime_InlineCache_cache_at_) 
+    //TODO typecheck here
+    ((Runtime_InlineCache)self)->method = NATIVE_ARG(0); 
+    ((Runtime_InlineCache)self)->class  = NATIVE_ARG(1); 
+    RETURN_FROM_NATIVE(self);
 }
 
 /* ========================================================================= */
@@ -39,4 +51,5 @@ void post_init_Runtime_InlineCache()
 {
     Collection_Dictionary natives = add_plugin(L"Runtime.InlineCache");
     store_native(natives, SMB_checkCached_, NM_Runtime_InlineCache_checkCached_);
+    store_native(natives, SMB_cache_at_, NM_Runtime_InlineCache_cache_at_);
 }
