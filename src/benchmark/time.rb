dda06a368a91b6e322c8009483b992b9611f7635
cami <cami@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2010-04-29 21:13:33 +0000

    more work on a small benchmark framework
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@1387 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- src/benchmark/time.rb
+++ src/benchmark/time.rb
@@ -25,10 +25,11 @@ end
 
 class Float
     def precision
+        return 10 if self == 0
         v = self.to_s
         precision = 0
         v.each_char { |c|
-            return precision unless c == '0' or c == '.'
+            return [1, precision].max unless c == '0' or c == '.'
             precision += 1
         }
     end
@@ -42,16 +43,16 @@ cmd = ARGV[1]
 
 probes = []
 
+#`(time -p #{cmd})`
 count.times {
-    a = `(time ./fibRuby.rb) 2>&1 | grep real`
+    a = `(time -p #{cmd}) 2>&1 | grep real`
     time =  a.match(/([0-9]+\.[0-9]+)/)[1].to_f
-    time += a.match(/([0-9]+)m/)[1].to_f * 60
+    #time += a.match(/([0-9]+)m/)[1].to_f * 60
     probes.push(time)
 }
 
 # =============================================================================
 stdev = probes.stdev
 format = "%.#{stdev.precision}f"
-
 value = sprintf(format+" ±"+format, probes.mean, stdev)
 puts "#{count} #{value}"
