4c689aa765c4ba570851b700c17cfed3c42e8278
cami <cami@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2010-05-18 06:56:09 +0000

    writing a clean benchmarking interface in ruby
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@1467 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- src/benchmark/benchmark.rb
+++ src/benchmark/benchmark.rb
@@ -19,51 +19,292 @@ class File
    end
    def pprint(arg1, *rest)
         Kernel.print(arg1, *rest)
+        $stdout.flush
         self.print(arg1, *rest)
    end
 end
 
+
 # ============================================================================
-def header(f)
-    f.puts
-    f.pputs "="*79
-#TODO branch for OSX uname
-    f.pputs `date`
-    f.pputs `uname -srp` if is_mac?
-    f.pputs `uname -srpi` if is_linux?
-    # TODO check for svn / git repos
-    f.pputs `git svn info | grep Revision`
-    f.pputs "="*79
+class Array
+    def sum
+        self.inject( nil ) { |sum,x| 
+            sum ? sum+x : x 
+        }
+    end
+
+    def mean
+        1.0 * self.sum / self.size
+    end
+    
+    def stdev
+        sq = 0
+	    m = self.mean
+    	self.each { |i|
+		    sq += (i-m)*(i-m)
+        }
+        sq /= self.length
+	    return Math.sqrt(sq)
+    end
 end
 
+class Float
+    def precision
+        return 10 if self == 0
+        v = self.to_s
+        precision = 0
+        v.each_char { |c|
+            return [1, precision].max unless c == '0' or c == '.'
+            precision += 1
+        }
+    end
+end
 
-# ============================================================================
-def benchmarks(f, list)
-    maxNameLength = 0
-    list.each { |l|
-        maxNameLength = [maxNameLength, l[0].length].max    
-        l.insert(1, '.') unless l.length == 3
-    }
-    list.each { |l|
-        f.pprint "#{l[0].chomp.ljust maxNameLength} "
-        run   = `cd #{l[1]}; #{Dir.pwd}/../time.rb 10 #{l[2]}`.chomp
-        parse = `cd #{l[1]}; #{Dir.pwd}/../time.rb 10 #{l[2]}`.chomp
-    }
+# =============================================================================
+class ScriptTest
+    attr_reader :script, :probeCount, :min, :max, :mean, :stdev, :dir
+    attr_writer :script, :probeCount, :dir
+
+    def initialize(script, probeCount=10, useUserTime=false)
+        @script      = script
+        @probeCount  = probeCount
+        @useUserTime = useUserTime
+        @dir         = Dir.pwd 
+    end
+
+    def reset
+        @probes = []
+        @min    = 0
+        @max    = 0
+        @mean   = 0
+        @stdev  = 0
+    end
+
+    def run
+        self.reset
+        self.probe
+        self.stats
+    end
+    
+    def probe
+        puts "(time #{@script}) 2>&1"
+        #TODO check for the executable dir here
+        @probeCount.times {|i|
+            a     = `(time #{@script}) 2>&1`
+            next unless $?.success?
+            if @useUserTime
+                a = a.grep(/user/).first
+            else 
+                a = a.grep(/real/).first
+            end
+            time  = a.match(/([0-9]+\.[0-9]+)/)[1].to_f
+            time += $1.to_f * 60 if a.match(/([0-9]+)m/)
+            @probes.push(time)
+        }
+    end
+
+    def stats
+        return if @probes.empty?
+        @mean  = @probes.mean
+        @stdev = @probes.stdev
+        @min   = @probes.min 
+        @max   = @probes.max
+    end
+
+    def empty?
+        @probes.nil? or @probes.empty?
+    end
+
+    def to_s
+        return "0" if @probes.nil?
+        return "#{@probes.length}" if self.empty?
+        format = "%.#{@stdev.precision}f"
+        value  = sprintf(format+"   ±   "+format, @mean, @stdev)
+        "#{@probes.length}  #{value}" 
+    end
 end
 
 # ============================================================================
-def runBenchmarks()
-    def dir 
-        begin 
-            return File.readlink $0
-        rescue
-            return $0
+class Test
+
+    def self.createFrom(description)
+        version = nil
+        if description.length == 4
+            version = description.shift
+        end
+        return self.new(description[0], description[1], description[2], version)
+    end
+
+    # ------------------------------------------------------------------------
+    attr_reader :executable, :testFile, :parseFile, :mean, :stdev, :min, :max,
+                :dir
+    attr_writer :dir
+
+    def initialize(executable, testFile, parseFile=nil, version=nil)
+        @executable = executable
+        @testFile   = testFile
+        @parseFile  = parseFile
+        @version    = version
+        @dir        = Dir.pwd
+        @version    = @version.chomp unless @version.nil?
+        self.version
+    end
+
+    def version
+        return @version               unless @version.nil?
+        return @executable.capitalize unless self.executableExists?
+        return `#{@executable} --version 2>&1`.chomp
+    end
+
+    def reset
+        @skipped        = false
+        @testFileProbe  = nil
+        @parseFileProbe = nil
+        @mean           = 0
+        @stdev          = 0
+        @min            = 0
+        @max            = 0
+    end
+
+    def run
+        self.reset
+        if not self.executableExists?
+            @skipped = true
+            return
+        end
+        if @parseFile.nil?
+            self.runSingle      
+        else
+            self.runBoth
+        end
+    end
+
+    def executableExists?
+        not `which #{@executable}`.chomp.empty?
+    end
+
+    def runBoth
+        self.runSingle
+        @parseFileProbe = self.runScriptTest(@parseFile)
+        if @parseFileProbe.empty?
+            @skipped = true
+        end
+        @mean  -= @parseFileProbe.mean
+        @stdev += @testFileProbe.stdev
+        @min   -= @testFileProbe.min
+        @max   -= @testFileProbe.max
+    end
+
+    def runSingle
+        @testFileProbe = self.runScriptTest(@testFile)
+        if @testFileProbe.empty?
+            @skipped = true
+        end
+        @mean  = @testFileProbe.mean
+        @stdev = @testFileProbe.stdev
+        @min   = @testFileProbe.min
+        @max   = @testFileProbe.max
+    end
+        
+    def runScriptTest(script)
+        script    = "#{@dir}/#{script}" unless script[0] == '/'
+        probe     = ScriptTest.new("#{@executable} #{script}")
+        probe.dir = @dir
+        probe.run
+        return probe
+    end
+
+    def to_s
+        return "--skipped--" if @skipped
+        format = "%.#{@stdev.precision}f"
+        sprintf("#{format} ± #{format}    %f    %f", @mean, @stdev, @min, @max)
+    end
+end
+    
+# ============================================================================
+class Benchmark
+
+    def self.createFrom(description)
+        benchmark = self.new
+        benchmark.load description
+        return benchmark
+    end
+
+    # ------------------------------------------------------------------------
+    def initialize(tests=[], dir=nil)
+        @tests, @dir = tests, dir
+        @dir   = Dir.pwd if @dir.nil?
+    end
+    
+    def load(description)
+        description.each { |array|
+            self.add Test.createFrom(array)
+        }
+    end
+
+    # ------------------------------------------------------------------------
+    def add(test)
+        @tests.push test
+        test.dir = @dir
+    end
+
+    def size
+        @tests.size
+    end
+
+    def empty?
+        @tests.empty?
+    end
+
+    def svn?
+        File.exists? "#{@dir}/.svn"    
+    end
+
+    # ------------------------------------------------------------------------
+    def header
+        @resultsFile.puts
+        @resultsFile.pputs "="*79
+        @resultsFile.pputs `date`
+        @resultsFile.pputs `uname -srp`  if is_mac?
+        @resultsFile.pputs `uname -srpi` if is_linux?
+        if self.svn?
+            @resultsFile.pputs `svn info | grep Revision`
+        else
+            @resultsFile.pputs `git svn info | grep Revision`
+        end
+        @resultsFile.pputs "="*79
+    end
+
+    def benchmarks
+        maxVersionNameLength = 0
+        @tests.each { |test|
+            maxVersionNameLength = [maxVersionNameLength, test.version.length].max    
+        }
+        @tests.each { |test|
+            @resultsFile.pprint "#{test.version.ljust maxVersionNameLength} "
+            test.run 
+            @resultsFile.pputs test.to_s
+        }
+    end
+
+    def changeDir
+        def dir 
+            begin 
+                return File.readlink $0
+            rescue
+                return $0
+            end 
         end 
-    end 
-    Dir.chdir File.dirname dir 
-    File.open("results/#{`uname -n`.chomp}.txt", 'a') { |f|
-        header(f)
-        benchmarks(f, BENCHMARKS) 
-    }
+        Dir.chdir File.dirname dir 
+    end
+
+    def run
+        self.changeDir
+        File.open("results/#{`uname -n`.chomp}.txt", 'a') { |f|
+            @resultsFile = f
+            self.header
+            self.benchmarks 
+        }
+    end
 end
 
--- src/benchmark/fib/run.rb
+++ src/benchmark/fib/run.rb
@@ -1,40 +1,24 @@
 #! /usr/bin/env ruby
 
-B1 = [
-    ['Python 2.6 Parse', 'python2.6 fibParse.py'],
-    ['Python 2.6',       'python2.6 fib.py'],
-
-    ['Python 3.0 Parse', 'python3.0 fibParse.py'],
-    ['Python 3.0',       'python3.0 fib.py'],
-
-    ['Python 3.1 Parse', 'python3.1 fib.py'],
-    ['Python 3.1',       'python3.1 fib.py'],
-
-    ['Ruby Parse',       './fibParse.rb'],
-    ['Ruby',             './fib.rb'],
-
-    ['Ruby 1.9 Parse',   'ruby1.9 ./fibParse.rb'],
-    ['Ruby 1.9',         'ruby1.9 ./fib.rb'],
-
-    ['Ruby 1.9.1 Parse', 'ruby1.9.1 ./fibParse.rb'],
-    ['Ruby 1.9.1',       'ruby1.9.1 ./fib.rb'],
-]
+require '../benchmark'
 
 BENCHMARKS = [
-    [`python2.6 --version`, 'python2.6', 'fibParse.py', 'fib.py'],
-    [`python3.0 --version`, 'python2.6', 'fibParse.py', 'fib.py'],
-    [`python3.1 --version`, 'python2.6', 'fibParse.py', 'fib.py'],
+    #['python2.6', 'fib.py', 'fibParse.py'],
+    #['python3.0', 'fib.py', 'fibParse.py'],
+    #['python3.1', 'fib.py', 'fibParse.py'],
     
-    [`ruby1.8 --version`,   'ruby1.8',   'fibParse.rb', 'fib.rb'],
-    [`ruby1.9 --version`,   'ruby1.9',   'fibParse.rb', 'fib.rb'],
-    [`ruby1.9.1 --version`, 'ruby1.9.1', 'fibParse.rb', 'fib.rb'],
+    #['ruby',      'fib.rb', 'fibParse.rb'],
+    #['ruby1.9',   'fib.rb', 'fibParse.rb'],
+    #['ruby1.9.1', 'fib.rb', 'fibParse.rb'],
     
-    [`gst --version | grep version`, 'gst -f', 'fibParse.gst', 'fib.gst'],
+    #[`gst --version | grep version`, 'gst -f', 'fib.gst', 'fibParse.gst'],
 
-    ['Pinocchio', '../../pinocchio', 'benchmark/fib/fibParse.p' 
-                                     'benchmark/fib/fib.p'],
+    ['Pinocchio', '../../pinocchio', 'fib.p', 'fibParse.p'],
 ]
 
+
+benchmark = Benchmark.createFrom BENCHMARKS
+
 if __FILE__ == $0
-    runBenchmarks()
+    benchmark.run
 end
