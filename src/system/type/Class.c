9db3ea8f777650a3287cbec67fd8b445c4a82d85
cami <cami@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2010-06-01 15:53:12 +0000

    adding class printout to DNU
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@1522 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- src/system/type/Class.c
+++ src/system/type/Class.c
@@ -87,7 +87,8 @@ void does_not_understand(Object self, Type_Class class, Object msg, uns_int argc
     if (msg == (Object)SMB_doesNotUnderstand_) {
         Runtime_Message message = (Runtime_Message)pop_EXP();
         // For now keep on printing the message.
-        fwprintf(stderr, L"\033[33mDNU: %ls\033[0m\n",
+        fwprintf(stderr, L"\033[33mDNU: %ls >> %ls\033[0m\n",
+                 class->name->value,
                  message->selector->value);
         fail(Exception_RecursiveDoesNotUnderstand_Class, 3,
              self, class, message);
