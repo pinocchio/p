562e63a289d8db849c3325bc53e971f4eadadcc9
tverwaes <tverwaes@475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc>
2010-06-12 14:45:50 +0000

    adding basic to_do_ optimization
    git-svn-id: http://pinocchio.unibe.ch/svn/pinocchio@1574 475e7f6d-5e0e-419c-b4a3-23a65b5ccdbc

--- src/system/interpretation/Threaded.c
+++ src/system/interpretation/Threaded.c
@@ -244,6 +244,31 @@ THREADED(sendn)
 }
 
 /* ========================================================================= */
+THREADED(send_to_do_) 
+    Optr from = PEEK_EXP(1);
+    Optr to   = PEEK_EXP(0);
+    if (HEADER(from) == SmallInt_Class && HEADER(to) == SmallInt_Class) {
+        ZAPN_EXP(2);
+        set_pc(pc + 3);
+        Block block = (Block)threaded_code()->values[pc + 2];
+        int start   = unwrap_int(from);
+        int stop    = unwrap_int(to);
+        for (;start<=stop; start++) {
+            PUSH_CNT_RAW(block->threaded);
+            PUSH_CNT_RAW(2); // pc
+            PUSH_CNT(eval_threaded);
+            PUSH_EXP(wrap_int(start));
+        }
+        return -1;
+    } else {
+        Send send = (Send)threaded_code()->values[pc + 1];
+        POKE_EXP(1, send);
+        POKE_EXP(0, from);
+        PUSH_EXP(to);
+        t_push_closure(pc + 1);
+        return t_send2(pc + 2);
+    }
+}
 
 THREADED(send_ifTrue_) 
     Optr bool = PEEK_EXP(0);
@@ -261,6 +286,28 @@ THREADED(send_ifTrue_)
         PUSH_EXP(send);
     	Class_dispatch(bool, HEADER(bool), 1);
         return -1;
+    }
+}
+
+THREADED(send_ifFalse_) 
+    Optr bool = PEEK_EXP(0);
+    if (bool == (Optr) false) {
+        ZAP_EXP();
+        set_pc(pc + 3);
+        Block block = (Block)threaded_code()->values[pc + 2];
+        PUSH_CNT_RAW(block->threaded);
+        PUSH_CNT_RAW(0); // pc
+        PUSH_CNT(eval_threaded);
+        return -1;
+    } else if (bool == (Optr) true) {
+        POKE_EXP(0, nil);
+        return pc + 3;
+    } else {
+        Send send = (Send)threaded_code()->values[pc + 1];
+        POKE_EXP(0, send);
+        PUSH_EXP(bool);
+        t_push_closure(pc + 1);
+        return t_send1(pc + 2);
     }
 }
 
@@ -390,7 +437,9 @@ void post_init_Threaded()
     T_FUNC(send4)
     T_FUNC(send5)
     T_FUNC(sendn)
+    T_FUNC(send_to_do_)
     T_FUNC(send_ifTrue_)
+    T_FUNC(send_ifFalse_)
     T_FUNC(send_ifTrue_ifFalse_)
 
     T_FUNC(super0)
