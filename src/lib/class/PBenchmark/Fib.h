40c2b332ae190ada60a5f2d8f9258696befacc5f
Camillo Bruni <camillobruni@gmail.com>
2010-11-05 18:34:04 +0100

    adding benchmark sources

---
+++ src/lib/class/PBenchmark/Fib.h
@@ -0,0 +1,15 @@
+#ifndef PINOCCHIO_LIB_PBENCHMARK_PBFIB
+#define PINOCCHIO_LIB_PBENCHMARK_PBFIB 
+
+#include <pinocchio.h>
+extern void init_PBenchmark_PBFib_methods();
+
+extern void init_PBenchmark_PBFib_layout();
+
+#include <lib/lib.h>
+
+extern Optr layout_PBenchmark_Fib_Class_class;
+extern Optr layout_PBenchmark_Fib;
+
+
+#endif //PINOCCHIO_LIB_PBENCHMARK_PBFIB
