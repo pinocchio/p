16ec890889f83194e595dff5ad148ad7b929e2a1
Camillo Bruni <camillobruni@gmail.com>
2011-03-10 22:36:34 +0100

    fixed new_Array_with

--- src/kernel/collection/array.c
+++ src/kernel/collection/array.c
@@ -10,7 +10,14 @@ Array new_Array_with(uns_int size, ...)
 	if (size == 0) {
 		return empty_array;
 	}
+    va_list args;
+    va_start(args, size);
 	NEW_ARRAYED(Array, Object, size);
+    uns_int i;
+    for (i = 0; i < size; i++) {
+        result->data[i] = va_arg(args, void**);
+    }
+    va_end(args);
 	return result;
 }
 
