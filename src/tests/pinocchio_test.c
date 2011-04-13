573feb7dd613af3b64e05279c75ebffa979d8f82
o <o@immerda.ch>
2011-04-14 00:14:46 +0200

    remove unused test macros

--- src/tests/pinocchio_test.c
+++ src/tests/pinocchio_test.c
@@ -11,21 +11,6 @@
 void mock_assert(const int result, const char* expression, const char *file,
                  const int line);
 
-/* Redirect calloc and free to test_calloc() and test_free() so cmockery can
- * check for memory leaks. */
-#ifdef calloc
-#undef calloc
-#endif // calloc
-#define calloc(num, size) _test_calloc(num, size, __FILE__, __LINE__)
-#ifdef free
-#undef free
-#endif // free
-#define free(ptr) _test_free(ptr, __FILE__, __LINE__)
-void* _test_calloc(const size_t number_of_elements, const size_t size,
-                   const char* file, const int line);
-void _test_free(void* const ptr, const char* file, const int line);
-
-
 int main(int argc, char* argv[]) {
 	pinocchio_bootstrap();
 
