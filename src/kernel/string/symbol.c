5f91594daebe6d54273532271012a632d87d6e19
Toon Verwaest <toon.verwaest@gmail.com>
2011-04-15 15:02:29 +0200

    small changes to the symbol table code

--- src/kernel/string/symbol.c
+++ src/kernel/string/symbol.c
@@ -16,12 +16,12 @@ static void SymbolTable_grow(SymbolTable table)
 
     BucketArray old_buckets = table->buckets;
 
-    uns_int newbit = SIZE(old_buckets);
+    uns_int newbit      = SIZE(old_buckets);
     BucketArray buckets = new_BucketArray_sized(newbit << 1);
-    table->buckets = buckets;
+    table->buckets      = buckets;
 
     uns_int bucket_idx;
-    uns_int limit = SIZE(old_buckets);
+    uns_int limit       = SIZE(old_buckets);
 
     for (bucket_idx = 0; bucket_idx < limit; bucket_idx++) {
         Bucket bucket = old_buckets->bucket[bucket_idx];
@@ -32,21 +32,22 @@ static void SymbolTable_grow(SymbolTable table)
         uns_int idx         = 0;
         uns_int newcount    = 0;
         uns_int bucket_size = bucket->tally->value;
+        Object * value      = bucket->value;
 
         while (idx < bucket_size) {
-            Symbol key = (Symbol)bucket->value[idx];
+            Symbol key = (Symbol)value[idx];
 
             if (HASH(key) & newbit) {
-                bucket_size -= 1;
-                newcount += 1;
-                bucket->value[idx]   = bucket->value[bucket_size];
-                bucket->value[bucket_size]   = (Object)key;
+                bucket_size       -= 1;
+                newcount          += 1;
+                value[idx]         = value[bucket_size];
+                value[bucket_size] = (Object)key;
             } else {
                 idx += 1;
             }
         }
-        bucket->tally = new_SmallInteger(bucket_size);
 
+        bucket->tally = new_SmallInteger(bucket_size);
         buckets->bucket[bucket_idx] = bucket;
 
         if (newcount > 0) {
@@ -55,14 +56,13 @@ static void SymbolTable_grow(SymbolTable table)
                 new_bucket = bucket;
                 buckets->bucket[bucket_idx] = (Bucket)nil;
             } else {
-                new_bucket = new_Bucket_sized(newcount << 2);
+                new_bucket = new_Bucket_sized(newcount << 1);
                 for (idx = 0; idx < newcount; idx += 1) {
                     new_bucket->value[idx] = bucket->value[bucket_size + idx];
                 }
             }
             new_bucket->tally = new_SmallInteger(newcount);
             buckets->bucket[limit+bucket_idx] = new_bucket;
-
         }
     }
 }
