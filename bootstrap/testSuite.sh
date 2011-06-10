4b245f7d0186e2b02166622e798b39a84049c404
Toon Verwaest <toon.verwaest@gmail.com>
2011-06-10 18:52:47 +0200

    adding simplemethod as a test

--- bootstrap/testSuite.sh
+++ bootstrap/testSuite.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 gcc -c -O3 plib.c
+./compile.sh simpleMethod $1 && ./a.out | tail -n 1
 ./compile.sh blockTest $1 && ./a.out | tail -n 1
 ./compile.sh blockTest2 $1 && ./a.out | tail -n 1
 ./compile.sh fib $1 && ./a.out | tail -n 1
