d04f19312aae120f3cce46ff33773a54dbe5bfa8
o <o@immerda.ch>
2011-07-25 15:55:16 +0200

    ARE_INTS can be a char, but unsigned

--- src/pinocchio.h
+++ src/pinocchio.h
@@ -51,7 +51,7 @@ extern struct Class Array;
 #define ENC_INT(v)  (((v) << 1) + 1)
 #define DEC_INT(v)  ((unsigned long)(v) >> 1)
 #define IS_INT(v)   ((unsigned char)(long)(v) & 1)
-#define ARE_INTS(x, y) ((long)(x) & (long)(y) & (char)1)
+#define ARE_INTS(x, y) ((unsigned char)(x) & (unsigned char)(y) & (char)1)
 
 #define PINOCCHIO_FAIL(msg) printf(""#msg"\n"); __asm("int3"); return 0 
 #define CLASS_OF(object)    (((long)object) & 1?&SmallInteger:((tClass*)object)[-2])
