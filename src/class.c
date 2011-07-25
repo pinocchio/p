0dd5eb69da83669145f7bce954f718d7aa7750fd
Toon Verwaest <toon.verwaest@gmail.com>
2011-07-25 12:46:57 +0200

    moving code away from hacks to class.c

--- src/class.c
+++ src/class.c
@@ -12,3 +12,39 @@ tMethod lookup(tObject receiver, tSymbol message)
     }
     return (tMethod)IdentityDictionary_lookup(methods, (tObject)message);
 }
+
+/* ======================================================================= */
+
+tObject basicNew(tBehavior b) {
+    long _h = b->instanceHeader >> 1;
+    tHeader h = *(tHeader*)&_h;
+    tObject result;
+    if (h.variable) {
+        result = (tObject)GC_MALLOC(sizeof(void**) * (h.base + 3)) + 3;
+        result[-3] = (tObject)0;
+    } else {
+        result = (tObject)GC_MALLOC(sizeof(void**) * (h.base + 2)) + 2;
+    }
+    result[-2] = (tObject)b;
+    result[-1] = (tObject)_h;
+    return result;
+}
+
+tObject basicNew_(tBehavior b, long tagged_size) {
+    if (!(tagged_size & 1)) {
+        PINOCCHIO_FAIL("Invalid size type given, expects SmallInteger");
+    }
+    long size = tagged_size >> 1;
+    long _h   = b->instanceHeader >> 1;
+    tHeader h = *(tHeader*)&_h;
+    tObject result;
+    if (h.variable) {
+        result = (tObject)GC_MALLOC(sizeof(void**) * (size + h.base + 3)) + 3;
+        result[-3] = (tObject)size;
+    } else {
+        PINOCCHIO_FAIL("Trying to instantiate non-variable-sized class");
+    }
+    result[-2] = (tObject)b;
+    result[-1] = (tObject)_h;
+    return result;
+}
--- src/hacks/plib.c
+++ src/hacks/plib.c
@@ -185,40 +185,6 @@ void print_object(void* object[]) {
     printf("\n");
 }
 
-tObject basicNew(tBehavior b) {
-    long _h = b->instanceHeader >> 1;
-    tHeader h = *(tHeader*)&_h;
-    tObject result;
-    if (h.variable) {
-        result = (tObject)GC_MALLOC(sizeof(void**) * (h.base + 3)) + 3;
-        result[-3] = (tObject)0;
-    } else {
-        result = (tObject)GC_MALLOC(sizeof(void**) * (h.base + 2)) + 2;
-    }
-    result[-2] = (tObject)b;
-    result[-1] = (tObject)_h;
-    return result;
-}
-
-tObject basicNew_(tBehavior b, long tagged_size) {
-    if (!(tagged_size & 1)) {
-        PINOCCHIO_FAIL("Invalid size type given, expects SmallInteger");
-    }
-    long size = tagged_size >> 1;
-    long _h   = b->instanceHeader >> 1;
-    tHeader h = *(tHeader*)&_h;
-    tObject result;
-    if (h.variable) {
-        result = (tObject)GC_MALLOC(sizeof(void**) * (size + h.base + 3)) + 3;
-        result[-3] = (tObject)size;
-    } else {
-        PINOCCHIO_FAIL("Trying to instantiate non-variable-sized class");
-    }
-    result[-2] = (tObject)b;
-    result[-1] = (tObject)_h;
-    return result;
-}
-
 void basicAtPut(tObject receiver, tSmallInteger index, tObject value)
 {
 }
