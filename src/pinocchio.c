3bed9c4bef1cdc0cb1422063f4734e83483848e2
o <o@immerda.ch>
2011-08-09 16:31:46 +0200

    also test doesNotUnderstand

--- src/pinocchio.c
+++ src/pinocchio.c
@@ -8,17 +8,26 @@ extern struct Class Test_KernelTest;
 extern struct Class Test_StaticKernelTest;
 extern struct Class Kernel_Package_Package;
 extern void * s_ok;
+extern void * s_print;
 extern void * invokeP();
 
 int main(int argc, char** argv)
 {
     setlocale(LC_ALL, "");
 
+    puts("");
     puts( "testing invokeP" );
     tObject self = basicNew((tBehavior)&Test_KernelTest);
     __asm("mov %0, %%rdi"::"r"(self));
     __asm("mov %0, %%rax"::"r"(&s_ok));
     __asm("call invokeP");
+    
+    puts("");
+    puts( "sending an unknown message:" );
+    __asm("mov %0, %%rdi"::"r"(self));
+    __asm("mov %0, %%rax"::"r"(&s_print));
+    __asm("call invokeP");
+    puts("");
     
     // tObject self    = basicNew((tBehavior)&Kernel_Package_Package);
     self = basicNew((tBehavior)&Test_StaticKernelTest);
