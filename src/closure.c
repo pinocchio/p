02020d78b8aef602d0b5711c7dc8ad4a8c85fcca
o <o@immerda.ch>
2011-08-31 16:48:41 +0200

    closureValue is now inlined by the compiler

--- src/closure.c
+++ src/closure.c
@@ -15,17 +15,6 @@ tObject remoteArrayNew(long size) {
     return array;
 }
 
-void closureValue() {
-    //if its not a closure then jmp to invoke
-    __asm("bt $0, %rdi");
-    __asm("jnae "FN_PREFIX"invoke");
-    __asm("cmp %0, -0x10(%%rdi)"::"r"(&Kernel_Behavior_Closure));
-    __asm("jne "FN_PREFIX"invoke");
-    //load code-pointer from the closure-object
-    __asm("mov (%rdi), %rax");
-    __asm("jmpq *%rax");
-}
-
 void closureReturn() {
   __asm("mov (%rdi),%rsp");
   
